ba7182a49a9ecfad6f3f962cc836cbc9d471a739
Pedro Ivo Bueno Sartório <pedroivobu@gmail.com>
2022-06-01 15:51:17 -0300

    Completing login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, session, redirect, flash, url_for, abort
 import sqlite3
 
 DATABASE = 'database.bd'
@@ -26,7 +26,36 @@ def display_entries():
     entries = []
     for title, wording, created_at in cur.fetchall():
         entries.append({"title": title, "wording": wording, "created_at": created_at})
-    return render_template("layout.html", entries=entries)
+    return render_template("display_entries.html", entries=entries)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logged'] = True
+            flash("Login successful!")
+            return redirect(url_for('display_entries'))
+        error = "User or Password incorrect"
+    return render_template("login.html", error=error)
+
+@app.route("/logout")
+def logout():
+    session.pop('logged', None)
+    flash("Logout successful!")
+    return redirect(url_for('display_entries'))
+
+@app.route("/insert", methods=["POST"])
+def insert_entries():
+    if not session.get('logged'):
+        abort(401)
+    title = request.form['title']
+    wording = request.form['wording']
+    sql = "INSERT INTO entries(title, wording) VALUES (?, ?)"
+    g.bd.execute(sql, [title, wording])
+    g.bd.commit()
+    flash("New entry inserted successfully!")
+    return redirect(url_for('display_entries'))
 
 if __name__ == "__main__":
     app.run()
